bdf8a21a14da6907299dfa33554c4411c9d225cc
si <249428629@qq.com>
2018-12-25 23:38:25 +0800

    raw todo

--- src/TodoList.js
+++ src/TodoList.js
@@ -1,83 +1,55 @@
-import React, { Component, Fragment } from 'react'
-import Item from './item'
-import './style.css'
+import React, { Component } from 'react'
+import { Input, Button ,List} from 'antd';
+import 'antd/dist/antd.css';  
 
-class TodoList extends Component{
-// 存放数据
-constructor(props){
-		super(props);
-		this.state = {
-				inputValue : '',
-				list:['1', '2']
-		}
-		this.handleInputChange = this.handleInputChange.bind(this)
-		this.handleSubmit = this.handleSubmit.bind(this)
-		this.handleItemDelete = this.handleItemDelete.bind(this)
-}
-
-render(){
-	return (
-			<Fragment>
-				{/* this is comment */}
-				{
-					// comment also
-				}
-					<div> 
-						<label htmlFor="inputArea">请输入</label>
-						<input 
-							id="inputArea"
-							className="input"
-							value={this.state.inputValue}
-							onChange={this.handleInputChange}
-							ref={(refInput) => {this.refInput = refInput}} />
-						<button onClick = {this.handleSubmit}>submit</button>
-					</div>
-					<ul>
-							{
-								this.getItem()
-							}
-					</ul>
-			</Fragment>
-	)
-}
-
-getItem(){
-	return this.state.list.map((item, index) =>{
-		// return <li key={index}
-		// 				 onClick={this.handleItemDelete.bind(this,index)}>{item}</li>
-						//  dangerouslySetInnerHTML={{__html: item}}></li>  不会转义
-			return <Item 
-						content={item} 
-						index={index} 
-						deleteItem={this.handleItemDelete}
-						key={index}/>
-	})
-}
-
-handleInputChange(e){
-	// 新版推荐用函数, 异步
-	// this.setState({
-	// 	'inputValue': e.target.value
-	// })  
-	const value = e.target.value
-	this.setState(() => ({inputValue: value}))
-} 
-handleSubmit(){
-	this.setState((prevState) => {return {
-		list: [...prevState.list, prevState.inputValue],
-		inputValue: ''
-	}})
-}
-handleItemDelete(index){
-	// immutable, 不允许直接修改state
-	
-	this.setState((prevState)=>{
-		let listCopy = [...prevState.list];
-		listCopy.splice(index, 1);
-		return {list: listCopy}
-
-	})
-}
+class TodoList extends Component {
+    constructor( props ) {
+        super(props);
+        this.state = {
+            inputContent :'abc',
+            todoList :[1,2,3]
+        }
+        this.handleChange = this.handleChange.bind(this)
+        this.addItem = this.addItem.bind(this)
+    }
+    
+    render () {
+        return (
+            <div className="common-box">
+                <div style={{marginBottom:'20px'}}>
+                    <Input size="small" style={{width:'200px'}} value={this.state.inputContent} onChange={this.handleChange}/>
+                    <Button type="primary" size="small"  style={{marginLeft:'30px'}} onClick={this.addItem}>Submit</Button>
+                </div>
+                <List
+                    bordered
+                    style={{width:'400px'}}
+                    dataSource={this.state.todoList}
+                    renderItem={(item, index) => (<List.Item onClick={this.deleteItem.bind(this, index)}>{item}</List.Item>)}
+                    />
+            </div>
+        )
+    }
+    
+    handleChange(e){
+        this.setState({
+            inputContent:e.target.value
+        })
+    }
+    addItem(){
+        this.setState({
+            todoList: this.state.todoList.concat(this.state.inputContent),
+            inputContent: ''
+        })
+        
+    }  
+    deleteItem(index){
+        let newList = [...this.state.todoList]
+        newList.splice(index, 1)
+        
+        this.setState({  
+            todoList:  newList
+        })
+    }
 
 }
 
